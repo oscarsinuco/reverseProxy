15fac15aeb6317e7bdff6c2b66ce1d6eba6617ab
Oscar Sinuco <oscarsinuco@gmail.com>
2020-07-08 16:54:48 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const http = require('http')
+const fs = require('fs');
+const app = express()
+const port = process.env.PORT || 3000
+
+
+const archivo = 'file.mp3'
+
+const download_file = (url) => {
+    return new Promise((resolve, reject) => {
+        let file = fs.createWriteStream('./'+archivo);
+        http.get(url, function (response) {
+            response.on('data', function (chunk) {
+                file.write(chunk)
+            })
+            response.on('end', function () {
+                console.log('download file completed.')
+                resolve('File download completed.')
+            })
+        })
+    })
+}
+
+app.get('/', (req, res) => {
+    download_file(req.query.url)
+        .then(response => {
+            res.download('./'+archivo)
+        })
+        .catch(error => {
+            res.status(404).send({
+                error: 'El archivo no pudo ser encontrado'
+            })
+        })
+});
+app.listen(port, () => console.log(`Example app listening on port port!`))
